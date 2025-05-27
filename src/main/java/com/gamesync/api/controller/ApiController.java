b7f6f059c93771f8c575daa59acf4619bbe9cd51
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-05-27 17:15:57 -0300

    testes

---
+++ src/main/java/com/gamesync/api/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.gamesync.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
